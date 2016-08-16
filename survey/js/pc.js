16aca897891e8eb4f60bdabc7ea2f7ece1715421
Mrangge <mickshifangge@gmail.com>
2016-08-16 13:28:39 +0800

    修改pc页表格滚动问题

--- survey/js/pc.js
+++ survey/js/pc.js
@@ -139,5 +139,15 @@ $(function(){
 		}
 	});
 
+	//table滚动条
+	$('.table_main').niceScroll({ 
+		cursorcolor: "#000",//#CC0071 光标颜色 
+		cursoropacitymax: 1, //改变不透明度非常光标处于活动状态（scrollabar“可见”状态），范围从1到0 
+		touchbehavior: true, //使光标拖动滚动像在台式电脑触摸设备 
+		cursorwidth: "7px", //像素光标的宽度 
+		cursorborder: "0", //     游标边框css定义 
+		cursorborderradius: "5px",//以像素为光标边界半径 条
+		horizrailenabled:true
+	});
 	
 });
