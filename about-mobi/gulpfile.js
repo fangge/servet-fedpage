b6e32f3ff4cf4d3a4a16d7685588ac6e1535d6d3
mrfangge <fangyang1@yy.com>
2016-12-31 12:26:51 +0800

    相关修改

--- about-mobi/gulpfile.js
+++ about-mobi/gulpfile.js
@@ -3,5 +3,4 @@ var gulp = require('gulp'),
 
 require('./tasks/gulpfile-dev')(gulp, plugins)
 require('./tasks/gulpfile-build')(gulp, plugins)
-require('./tasks/gulpfile-publish')(gulp, plugins)
 require('./tasks/gulpfile-zip')(gulp, plugins)
